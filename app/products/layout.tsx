2fb4c049965b134035946c41d44f54c932c52a23
jayramkhose <jayramkhose6463@gmail.com>
2024-03-06 09:32:05 +0530

    second commit

---
+++ app/products/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import contactUs from '../../src/components/ContactUs'
+export const metadata: Metadata = {
+  title: 'Products',
+  description: 'Example product page with mock data',
+};
+
+export default function ProductsLayout({
+  children,
+
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return(
+<html lang='eng'>
+  <body>{children}</body>
+</html>
+  ) 
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+// import type { Product } from '@type/products';
+
+import ContactUs from '@/src/components/ContactUs';
+import { useState,useEffect } from 'react';
+export default function Products() {
+  
+  return (
+   <div className="App">
+     <ContactUs/>
+   </div>
+  );
+}
